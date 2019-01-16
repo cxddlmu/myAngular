4c92716ca3e6010ca94ed5cfd32cc8d82ac0772a
cxddlmu <cxd_dlmu@126.com>
2019-01-16 17:04:19 +0800

    Add files via upload

---
+++ src/app/shared/pipe/safePipe.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SafePipePipe } from './safePipe.pipe';
+
+describe('Pipe: SafePipee', () => {
+  it('create an instance', () => {
+    let pipe = new SafePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
