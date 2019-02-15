f3863d302be911daf51e37f3f7afb08d4856ef88
cxddlmu <cxd_dlmu@126.com>
2019-02-15 17:44:04 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(
+    private router: Router,
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        new Promise((resolve) => {
+          console.log("isLoading");
+          resolve(true);
+        })
+      } else if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) {
+        console.log("isLoading close");
+
+      }
+    }, (error: any) => {
+      console.log("error isLoading close");
+    });
+  }
 }
