eeb004a9c17a637c2c986c740beb9293a1009f24
cxddlmu <cxd_dlmu@126.com>
2019-02-19 17:50:14 +0800

    Add files via upload

---
+++ src/app/new-tech/fuse/fuse.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import Fuse from 'fuse.js'
+@Component({
+  selector: 'app-fuse',
+  templateUrl: './fuse.component.html',
+  styleUrls: ['./fuse.component.css']
+})
+export class FuseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    var books = [{
+      'title': "Old Man's War",
+      'author': 'John Scalzi',
+      'tags': ['fiction']
+    }, {
+      'title': 'The Lock Artist',
+      'author': 'Steve',
+      'tags': ['thriller']
+    }]
+    var options: Fuse.FuseOptions<any> = {
+      keys: ['author', 'tags']
+    };
+    var fuse = new Fuse(books, options)
+
+    console.log(fuse.search('tion'))
+  }
+
+}
