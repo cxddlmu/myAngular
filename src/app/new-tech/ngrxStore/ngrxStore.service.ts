a05a1fc408aa2ddc050430ca27f2f4fb815494b2
cxddlmu <cxd_dlmu@126.com>
2019-03-13 15:51:12 +0800

    Update ngrxStore.service.ts

--- src/app/new-tech/ngrxStore/ngrxStore.service.ts
+++ src/app/new-tech/ngrxStore/ngrxStore.service.ts
@@ -3,18 +3,43 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { catchError } from 'rxjs/internal/operators/catchError';
 import { map } from 'rxjs/internal/operators/map';
 import { mergeMap } from 'rxjs/internal/operators/mergeMap';
-import { EMPTY, Observable, Subject, of } from 'rxjs';
-
+import { EMPTY, Observable, Subject, of, BehaviorSubject } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { tap, take } from 'rxjs/operators';
+interface AppState {
+    count: number;
+}
 @Injectable()
 export class NgrxStoreService {
+    constructor(private store: Store<AppState>) {
 
+    }
+    count$: Observable<number>;
+    behaviorSubject: BehaviorSubject<number>;
     getRandomNumber() {
         console.log("NgrxStoreService.getAll");
-        return of(this.getRandomIntInclusive(1,10))
+        return of(this.getRandomIntInclusive(1, 10))
     }
     private getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
     }
+    subscribeCount(): Observable<any> {
+        let behaviorSubject = new BehaviorSubject(0);
+        // let subscripton = this.store.select('count').subscribe(val => { console.log("subscribeCount"+val); behaviorSubject.next(val) });
+        // subscripton.unsubscribe();
+        let subscripton = this.store.select('count').pipe(take(1)).subscribe(val => { console.log("subscribeCount"+val); behaviorSubject.next(val) });
+        return behaviorSubject;
+    }
+
+    subscribeCount1(): Observable<any> {
+        // return this.store.select('count').pipe(map(count=>count*2));
+        let subject = new Subject();
+        // let subscripton = this.store.select('count').subscribe(val => { console.log("subscribeCount"+val); behaviorSubject.next(val) });
+        // subscripton.unsubscribe();
+        let subscripton = this.store.select('count').pipe(take(1)).subscribe(val => { console.log("subscribeCount"+val); subject.next(val) });
+        return subject;
+    }
+    
 }
