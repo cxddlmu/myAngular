fccc4fc9f3066abd30718c98f3e3158896392cda
cxddlmu <cxd_dlmu@126.com>
2019-03-26 14:47:34 +0800

    Update rythm.component.ts

--- src/app/new-tech/rythm/rythm.component.ts
+++ src/app/new-tech/rythm/rythm.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rythm',
-  templateUrl: './rythm.component.html',
-  styleUrls: ['./rythm.component.css']
+  //templateUrl: './rythm.component.html',
+  //styleUrls: ['./rythm.component.css']
 })
 export class RythmComponent implements OnInit {
 
