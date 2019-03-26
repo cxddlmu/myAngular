18c0f9d5d93f44c516873f790f50b27d881dc9f1
cxddlmu <cxd_dlmu@126.com>
2019-03-26 18:09:20 +0800

    Update material2.module.ts

--- src/app/material2/material2.module.ts
+++ src/app/material2/material2.module.ts
@@ -3,17 +3,30 @@ import { CommonModule } from '@angular/common';
 import { Material2Component } from './material2.component';
 import { SharedModule } from '../shared/shared.module';
 import { Routes, RouterModule } from '@angular/router';
+import { Material2FormComponent } from './material2Form/material2Form.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 const routes: Routes = [
 
 
   {
     path: "", component: Material2Component,
+    children: [
+      {
+        path: "material2Form",
+        // canActivate: [ContentGuard],
+        component: Material2FormComponent,
+        data: {}
+      },
+    ]
   }
 ];
 @NgModule({
   imports: [
-    CommonModule,RouterModule.forChild(routes),SharedModule,
+    CommonModule, RouterModule.forChild(routes), SharedModule,FlexLayoutModule
   ],
-  declarations: [Material2Component]
+  declarations: [Material2Component,
+    Material2FormComponent
+  ]
 })
 export class Material2Module { }
