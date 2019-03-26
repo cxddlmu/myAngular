149b0936d7e4f7cda3d2bfd8fdac1938094eef50
cxddlmu <cxd_dlmu@126.com>
2019-03-26 14:47:20 +0800

    Create rythm.component.ts

---
+++ src/app/new-tech/rythm/rythm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rythm',
+  templateUrl: './rythm.component.html',
+  styleUrls: ['./rythm.component.css']
+})
+export class RythmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
