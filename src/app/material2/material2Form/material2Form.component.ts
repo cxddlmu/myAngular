f6cef780ab0de6e8a1cf220eee54e8a6f926aae9
cxddlmu <cxd_dlmu@126.com>
2019-03-28 18:22:35 +0800

    Update material2Form.component.ts

--- src/app/material2/material2Form/material2Form.component.ts
+++ src/app/material2/material2Form/material2Form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, ViewChild } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { Required } from "src/app/shared/annotation/model-annotation";
+import { Store } from "@ngrx/store";
+import { ActionTypes, insertDemo, BasicInfo, Demographic } from "src/app/new-tech/ngrxStore/ngrxStore.component";
 
 @Component({
   selector: "app-material2Form",
@@ -8,16 +10,37 @@ import { Required } from "src/app/shared/annotation/model-annotation";
   // styleUrls: ['./material2Form.component.css']
 })
 export class Material2FormComponent implements OnInit {
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store<any>) { }
 
   ngOnInit() {
-    this.personalInfoForm = this.fb.group({
+    this.basicInfo = this.fb.group({
       prefix: ["", Validators.required],
       firstName: [""],
-      lastName: [""]
+      lastName: [""],
+      customerTitle: [""],
+      saluteBy: [""],
+      aliasName: [""],
+      otherName: [""],
+      motherMaidenName: [""],
+      birthday: [""],
+      spokenLanguage: [""],
+      correspondenceLanguage: [""],
+      gender: [""],
+      maritalStatus: [""]
+    });
+    this.nationalityForm = this.fb.group({
+      'nationalityArray': this.fb.array([
+        this.fb.group({
+          'nationalityId': ['', Validators.required]
+        })
+      ], Validators.required),
     });
   }
-  personalInfoForm;
+  basicInfo;
+  nationalityForm;
+  demo: Demographic = {
+    basicInfo: this.basicInfo
+  }
   @Input() label: string;
   @Input() start: any;
   @Input() end: any;
@@ -27,7 +50,9 @@ export class Material2FormComponent implements OnInit {
 
   onSubmit() {
     console.log(this.domain);
-    console.log(this.personalInfoForm);
+    console.log(this.basicInfo);
+    this.store.dispatch(new insertDemo(this.demo));
+    this.store.select(ActionTypes.SelectDemo).subscribe(val => { console.log(">>>>>>>" + JSON.stringify(val)); })
   }
   // @ViewChild("domain.prefix") prefix: viewchi;
   domain = { prefix: "" };
